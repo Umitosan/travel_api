355d618f37cf9f098457ec316f71091b63cef6e8
Dominic Brown and Jason Ainsworth <jason@>
2017-07-17 16:47:38 -0700

    working site with tests

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :review do
-    content "testContent"
-    author "testAuthor"
+    content "testContent123"
+    author "testAuthor123"
   end
 end
---
+++ spec/requests/update_route_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "post a review route", :type => :request do
+  let!(:reviews) { FactoryGirl.create_list(:review, 5)}
+
+  # before { get '/reviews'}
+
+  it 'tests the put route' do
+    # binding.pry
+    thisReview = Review.all.first
+    put "/reviews/#{thisReview.id}", params: { :author => 'test_author', :content => 'test_content' }
+    expect(Review.find(thisReview.id).content).to eq ('test_content')
+  end
+
+end
